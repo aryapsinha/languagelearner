d3949615c011c49727791c587a4ba2ff59b84a2e
Shirley <elenagrenewherd7856@gmail.com>
2025-02-08 17:21:02 -0500

    added translated text component

--- scripts/popup scripts/update.js
+++ scripts/popup scripts/update.js
@@ -5,5 +5,6 @@ chrome.runtime.onMessage.addListener((message) => {
     if (message.type === 'highlight') {
         console.log('Updating popup with text:', message.text);
         document.getElementById('selected-text').textContent = message.text;
+        document.getElementById('translated-text').textContent = "Translating...";
     }
 });
