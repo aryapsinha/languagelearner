9519d9176ebd54b52816db663bb83a117b5fe774
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2025-02-08 17:47:34 -0500

    popup + react coexist

--- scripts/popup scripts/background.js
+++ scripts/popup scripts/background.js
@@ -6,23 +6,8 @@ let isPopupOpen = false;
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'highlight') {
         console.log('Message received:', message);
-        if (!isPopupOpen) {
-            console.log('Opening popup');
-            isPopupOpen = true;
-            chrome.action.openPopup(() => {
-                chrome.runtime.sendMessage({ type: 'highlight', text: message.text });
-            });
-        } else {
-            console.log('Popup already open');
+        chrome.action.openPopup(() => {
             chrome.runtime.sendMessage({ type: 'highlight', text: message.text });
-        }
+        });
     }
 });
-
-chrome.runtime.onConnect.addListener((port) => {
-    if (port.name === 'popup') {
-      port.onDisconnect.addListener(() => {
-        isPopupOpen = false;
-      });
-    }
-  });
